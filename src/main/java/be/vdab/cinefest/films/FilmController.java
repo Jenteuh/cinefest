8048d9aee65fd98389497f680d95a563c09e6f0f
Jente Huysmans <jentehuysmans@hotmail.com>
2025-07-04 15:37:15 +0200

    GET

---
+++ src/main/java/be/vdab/cinefest/films/FilmController.java
@@ -0,0 +1,20 @@
+package be.vdab.cinefest.films;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FilmController {
+
+    private final FilmService filmService;
+
+    FilmController(FilmService filmService) {
+        this.filmService = filmService;
+    }
+
+    @GetMapping("films/totaalvrijeplaatsen")
+    long findTotaalVrijePlaatsen() {
+        return filmService.findAantalVrijePlaatsen();
+    }
+
+}
