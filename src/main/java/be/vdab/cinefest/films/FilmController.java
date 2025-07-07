d067575044dd9fa6766a5cf147c786e8f7045652
Jente Huysmans <jentehuysmans@hotmail.com>
2025-07-07 17:20:48 +0200

    DTO

--- src/main/java/be/vdab/cinefest/films/FilmController.java
+++ src/main/java/be/vdab/cinefest/films/FilmController.java
@@ -13,14 +13,21 @@ public class FilmController {
         this.filmService = filmService;
     }
 
+    private record IdTitelJaarVrijePlaatsen(long id, String titel, int jaar, int vrijePlaatsen) {
+        IdTitelJaarVrijePlaatsen(Film film) {
+            this(film.getId(), film.getTitel(),  film.getJaar(), film.getVrijePlaatsen());
+        }
+    }
+
     @GetMapping("films/totaalvrijeplaatsen")
     int findTotaalVrijePlaatsen() {
         return filmService.findAantalVrijePlaatsen();
     }
 
     @GetMapping("/films/{id}")
-    Film findById(@PathVariable long id) {
+    IdTitelJaarVrijePlaatsen findById(@PathVariable long id) {
         return filmService.findById(id)
+                .map(film -> new IdTitelJaarVrijePlaatsen(film))
                 .orElseThrow(() -> new FilmNietGevondenException(id));
     }
 
---
+++ src/main/resources/static/js/film.js
@@ -0,0 +1,28 @@
+"use strict";
+import {byId, toon, verberg, setText} from "./util.js";
+byId("zoek").onclick = async () => {
+    verberg("film", "zoekIdFout", "nietGevonden", "storing");
+    const zoekIdInput = byId("zoekId");
+    if (!zoekIdInput.checkValidity()) {
+        toon("zoekIdFout");
+        zoekIdInput.focus();
+        return;
+    }
+    findById(zoekIdInput.value);
+};
+async function findById(id) {
+    const response = await fetch(`films/${id}`);
+    if (response.ok) {
+        const film = await response.json();
+        toon("film");
+        setText("titel", film.titel);
+        setText("jaar", film.jaar);
+        setText("vrijePlaatsen", film.vrijePlaatsen)
+    } else {
+        if (response.status === 404) {
+            toon("nietGevonden");
+        } else {
+            toon("storing");
+        }
+    }
+}
